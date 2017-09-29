e6fac4ec2f4fae59ccf226e63097389dca52f0c8
frobino <frobino@users.noreply.github.com>
2017-09-29 17:16:13 +0200

    Create a socket.io server and log connections
    - a socket.io server is created on top of the http server
    - the new io server is used to log connections/disconnections
    - a socket.io client is inserted in index.html, so a new client will
      connect every time a browser connects/talks to the server

--- socket-io-chat/index.js
+++ socket-io-chat/index.js
@@ -1,15 +1,25 @@
 // Initialize app to be a function handler...
 var app = require('express')();
-// ...ro be supplied to an HTTP server
+// ...to be supplied to an HTTP server
 var http = require('http').Server(app);
+// create socket.io server, mounting "on top of" the HTTP server
+var io = require('socket.io')(http);
 
-// Here is the function supplied to the server:
+// here is the function supplied to the HTTP server:
 app.get('/', function(req, res){
   // serve a html file when the function is executed
   res.sendFile(__dirname + '/index.html');
 });
 
-// Make the server start and listen
+// listen on connection/disconnection on a socket and log it
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+    console.log('user disconnected');
+  });
+});
+
+// make the HTTP server start and listen
 http.listen(3000, function(){
   console.log('listening on *:3000');
 });
